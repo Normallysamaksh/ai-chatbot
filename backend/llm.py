dffada3f2ec9fb37deb4e9ac8b30b462ce7e1e1b
Samaksh <samakshsingh127@gmail.com>
2026-07-12 23:57:57 +0530

    corrected bedrock model

--- backend/llm.py
+++ backend/llm.py
@@ -3,7 +3,7 @@ from langchain_aws import ChatBedrock
 load_dotenv()
 
 llm = ChatBedrock(
-    model_id="amazon.nova-lite-v1:0",
+    model_id="meta.llama3-70b-instruct-v1:0",
     model_kwargs={
         "temperature": 0,
     },
