34d66f2d3c68d3c13995b1e74c6bf5e53d945823
Samaksh <samakshsingh127@gmail.com>
2026-07-13 02:03:45 +0530

    retrieve and gen testing

--- backend/aws/test_kb.py
+++ backend/aws/test_kb.py
@@ -7,11 +7,20 @@ bedrock_agent_runtime = boto3.client(
     region_name="ap-south-1",
 )
 
-response = bedrock_agent_runtime.retrieve(
-    knowledgeBaseId="LOIP2RDSQ0",
-    retrievalQuery={
-        "text": "Summarise this document",
-    }
+response = bedrock_agent_runtime.retrieve_and_generate(
+    input={
+        "text": "summarise this document"
+    },
+    retrieveAndGenerateConfiguration={
+        "type": "KNOWLEDGE_BASE",
+        "knowledgeBaseConfiguration": {
+            "knowledgeBaseId": "LOIP2RDSQ0",
+            "modelArn": (
+                "arn:aws:bedrock:ap-south-1::foundation-model/"
+                "meta.llama3-70b-instruct-v1:0"
+            ),
+        },
+    },
 )
 
 print(response)
